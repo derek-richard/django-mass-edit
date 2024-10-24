7b67c121aad212b4835594eff0dcdcd1d2d46938
derek-richard <dhohls@creationbreeding.co.za>
2024-10-24 14:02:04 +0200

    Updated exception handler for Database errors on object save

--- massadmin/massadmin.py
+++ massadmin/massadmin.py
@@ -66,8 +66,9 @@ from . import settings
 
 def url_to_edit_object(obj):
     url = reverse('admin:%s_%s_change' % (
-        obj._meta.app_label, obj._meta.model_name), args=[obj.id])
-    return '<a href="%s" target="_">%s</a>' % (url,  obj.__unicode__())
+        obj._meta.app_label, obj._meta.model_name), args=[obj.pk])
+    return '<a href="%s" target="_">%s #%s</a>' % (
+        url, obj._meta.model_name, obj.pk)
 
 
 def mass_change_selected(modeladmin, request, queryset):
